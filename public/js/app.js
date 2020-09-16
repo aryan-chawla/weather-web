352eb7067c2b0b4be0cd7d40d371cb52b0308dce
Godpunes Hulksmash <aryanchawla.awesome1945@gmail.com>
2020-09-16 14:42:36 +0530

    remove unnnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('this is client side javascript')
-
 const weather = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
